c700625e594b1dae863487b352eaaa108556e5df
Dang Tri Hai <haic1947@gmail.com>
2025-06-05 15:40:41 +0700

    done

--- Thi/Models/LoginViewModel.cs
+++ Thi/Models/LoginViewModel.cs
@@ -8,11 +8,6 @@ namespace Thi.Models
         [Display(Name = "Mã sinh viên")]
         public string MaSV { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Mật khẩu là bắt buộc")]
-        [Display(Name = "Mật khẩu")]
-        [DataType(DataType.Password)]
-        public string MatKhau { get; set; } = string.Empty;
-
         [Display(Name = "Ghi nhớ đăng nhập")]
         public bool RememberMe { get; set; }
     }
